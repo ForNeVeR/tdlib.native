f4d9ea566402732b3980df6f29fbd04afa2f5e2c
Friedrich von Never <friedrich@fornever.me>
2018-09-02 18:58:39 +0700

    Update install scripts

--- install.linux.ps1
+++ install.linux.ps1
@@ -1,2 +1,4 @@
 sudo apt-get install gperf
 if (!$?) { throw 'Cannot install dependencies from apt-get' }
+
+git submodule update --init --recursive
--- install.windows.ps1
+++ install.windows.ps1
@@ -1,2 +1,4 @@
 choco install gperf
+if (!$?) { throw 'Cannot install dependencies from choco' }
+
 git submodule update --init --recursive
