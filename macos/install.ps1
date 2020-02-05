d8e1a8c11bbde040d3a367c9ec4375fa3ffabc4f
Friedrich von Never <friedrich@fornever.me>
2020-02-06 00:25:48 +0700

    Install .NET Core on macOS (#16)

--- macos/install.ps1
+++ macos/install.ps1
@@ -1,4 +1,7 @@
 $ErrorActionPreference = 'Stop'
 
-brew install gperf openssl nuget
+brew install gperf nuget openssl
 if (!$?) { throw 'Cannot install dependencies from brew' }
+
+brew cask install dotnet-sdk
+if (!$?) { throw 'Cannot install dotnet-sdk' }
