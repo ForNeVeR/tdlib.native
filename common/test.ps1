09b3ae5f97b4419f261388eb867dac4eb6b0b1e4
Friedrich von Never <friedrich@fornever.me>
2020-02-06 23:26:00 +0700

    Use the right .NET Core SDK on macOS to run the tests (#16)

--- common/test.ps1
+++ common/test.ps1
@@ -3,8 +3,12 @@ param (
     [string] $PackageSource = "$BuildRoot/../build/nuget/",
     [string] $TdSharpRoot = "$PSScriptRoot/../tdsharp",
     [string] $TdSharpTestProjectName = 'TDLib.Tests',
+    [string] $PackageName = 'tdlib.native',
 
-    [string] $NuGet = 'NuGet.exe'
+    [string] $NuGet = 'NuGet.exe',
+    [switch] $UseMono,
+    [string] $Mono = 'mono',
+    [string] $dotnet = 'dotnet'
 )
 
 $ErrorActionPreference = 'Stop'
@@ -12,16 +16,23 @@ Set-StrictMode -Version Latest
 
 New-Item -Type Directory $PackageSource -ErrorAction Ignore
 $package = Get-Item $BuildRoot/*.nupkg
+
+Write-Output "Adding a package $package into NuGet source $PackageSource"
 & $NuGet add $package -Source $PackageSource
 if (!$?) { throw 'Cannot add a NuGet package into source' }
 
 Push-Location $TdSharpRoot
 try {
-    dotnet remove $TdSharpTestProjectName package tdlib.native
+    Write-Output "Removing a package $PackageName from the project $TdSharpTestProjectName"
+    & $dotnet remove $TdSharpTestProjectName package $PackageName
     if (!$?) { throw 'Cannot uninstall package from the test project' }
-    dotnet add $TdSharpTestProjectName package tdlib.native --source $PackageSource
+
+    Write-Output "Adding a package $PackageName from the project $TdSharpTestProjectName"
+    & $dotnet add $TdSharpTestProjectName package $PackageName --source $PackageSource
     if (!$?) { throw 'Cannot add package into the test project' }
-    dotnet test
+
+    Write-Output "Running tests"
+    & $dotnet test --verbosity normal
     if (!$?) { throw 'Tests failed' }
 } finally {
     Pop-Location
--- macos/install.ps1
+++ macos/install.ps1
@@ -3,5 +3,8 @@ $ErrorActionPreference = 'Stop'
 brew install gperf nuget openssl
 if (!$?) { throw 'Cannot install dependencies from brew' }
 
-brew cask install dotnet-sdk
+brew tap isen-ng/dotnet-sdk-versions
+if (!$?) { throw 'Cannot execute brew tap' }
+
+brew cask install dotnet-sdk-2.2.100
 if (!$?) { throw 'Cannot install dotnet-sdk' }
