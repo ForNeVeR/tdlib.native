d61415f46defadb6bd9236f6d54b836fef77496f
Friedrich von Never <friedrich@fornever.me>
2020-02-16 19:29:40 +0700

    Check return codes from git submodule installation, add logs (#22)

--- macos/install.ps1
+++ macos/install.ps1
@@ -1,11 +1,14 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
+Write-Output 'Installing dependencies from brew'
 brew install gperf nuget openssl
 if (!$?) { throw 'Cannot install dependencies from brew' }
 
+Write-Output 'Performing brew tap'
 brew tap isen-ng/dotnet-sdk-versions
 if (!$?) { throw 'Cannot execute brew tap' }
 
+Write-Output 'Installing dotnet-sdk from cask'
 brew cask install dotnet-sdk-2.2.100
 if (!$?) { throw 'Cannot install dotnet-sdk' }
--- windows/install.ps1
+++ windows/install.ps1
@@ -1,7 +1,10 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
+Write-Output 'Installing gperf'
 choco install gperf
 if (!$?) { throw 'Cannot install dependencies from choco' }
 
+Write-Output 'Updating the Git submobules'
 git submodule update --init --recursive
+if (!$?) { throw 'Cannot update the Git submodules' }
