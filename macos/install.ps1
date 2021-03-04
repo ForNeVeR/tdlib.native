306bdc4d6129d515b93fa190b7182a2d6e01acde
Friedrich von Never <friedrich@fornever.me>
2021-03-04 23:11:56 +0700

    #24: get rid of Travis scripts

--- macos/install.ps1
+++ macos/install.ps1
@@ -1,14 +1,6 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
-Write-Output 'Installing dependencies from brew'
-brew install gperf nuget openssl
-if (!$?) { throw 'Cannot install dependencies from brew' }
-
-Write-Output 'Performing brew tap'
-brew tap isen-ng/dotnet-sdk-versions
-if (!$?) { throw 'Cannot execute brew tap' }
-
-Write-Output 'Installing dotnet-sdk from cask'
-brew cask install dotnet-sdk-2.2.100
-if (!$?) { throw 'Cannot install dotnet-sdk' }
+Write-Output 'Installing gperf from brew'
+brew install gperf
+if (!$?) { throw 'Cannot install gperf from brew' }
