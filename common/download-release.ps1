b279ad95e1155ed7439e421e57117aabaffdff35
P9oS <sandybaevr@yandex.ru>
2021-04-16 16:22:22 +0500

    update version 1.7.0

--- common/download-release.ps1
+++ common/download-release.ps1
@@ -1,5 +1,5 @@
 param (
-    [string] $Version = '1.6.0',
+    [string] $Version = '1.7.0',
     [string] $Tag = "v$Version",
     [string] $BaseAddress = "https://github.com/ForNeVeR/tdlib.native/releases/download/$Tag",
     [string] $BuildDirectory = "$PSScriptRoot/../build"
