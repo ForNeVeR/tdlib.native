9bfb88bbe30ab4606521df897f4661388a9dd4ae
P9oS <sandybaevr@yandex.ru>
2021-04-18 02:08:11 +0500

    change .gitmodules on fork repository && update submodule tdsharp && remove install netcore2.2-runtime from windows install script

--- windows/install.ps1
+++ windows/install.ps1
@@ -5,10 +5,6 @@ Write-Output 'Installing gperf'
 choco install gperf
 if (!$?) { throw 'Cannot install dependencies from choco' }
 
-Write-Output 'Installing .net core 2.2'
-choco install dotnetcore-2.2-runtime
-if (!$?) { throw 'Cannot install .net core 2.2' }
-
 Write-Output 'Updating the Git submobules'
 git submodule update --init --recursive
 if (!$?) { throw 'Cannot update the Git submodules' }
