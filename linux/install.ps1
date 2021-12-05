ed8178c8b509befd93bb9d337c937758a0d56387
Friedrich von Never <friedrich@fornever.me>
2021-12-05 20:24:43 +0700

    (#51) Linux: install all the required dependencies

--- linux/install.ps1
+++ linux/install.ps1
@@ -6,14 +6,23 @@ param (
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
-Write-Output 'Installing gperf'
-apt-get install gperf
+$dependencies = @(
+    'cmake'
+    'gperf'
+    'make'
+    'git'
+    'zlib1g-dev'
+    'libssl-dev'
+    'gperf'
+    'php-cli'
+    'cmake'
+    'g++'
+)
+
+Write-Output 'Installing dependencies.'
+apt-get install -y @dependencies
 if (!$?) { throw 'Cannot install dependencies from apt-get' }
 
 Write-Output "Downloading NuGet client to $NuGetPath"
 New-Item -Type Directory ([IO.Path]::GetDirectoryName($NuGetPath))
 Invoke-WebRequest -OutFile $NuGetPath $NuGetDownloadUrl
-
-Write-Output 'Updating the Git submobules'
-git submodule update --init --recursive
-if (!$?) { throw 'Cannot update the Git submodules' }
