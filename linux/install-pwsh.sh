76767aa19be90ee68b42b2e2e0f350115a8d46a3
Friedrich von Never <friedrich@fornever.me>
2021-12-05 20:04:41 +0700

    (#51) Linux: install PowerShell

---
+++ linux/install-pwsh.sh
@@ -0,0 +1,9 @@
+﻿set -e
+
+apt-get update && apt-get install wget
+
+cd /tmp
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+
+apt-get update && apt-get install powershell
