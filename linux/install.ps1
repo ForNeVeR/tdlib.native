c8fdf378381d0b94d0147cd7bfadfdd4828b9057
Friedrich von Never <friedrich@fornever.me>
2021-12-05 20:32:39 +0700

    (#51) Linux: forbid interactivity during apt-get

--- linux/install.ps1
+++ linux/install.ps1
@@ -6,22 +6,31 @@ param (
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
-$dependencies = @(
-    'cmake'
-    'gperf'
-    'make'
-    'git'
-    'zlib1g-dev'
-    'libssl-dev'
-    'gperf'
-    'php-cli'
-    'cmake'
-    'g++'
-)
+try {
+    # The following command requires installing tzdata which is interactive, so let's forbid its interactivity.
+    $oldDebianFrontend = $env:DEBIAN_FRONTEND
+    $env:DEBIAN_FRONTEND = 'noninteractive'
+    $dependencies = @(
+        'cmake'
+        'gperf'
+        'make'
+        'git'
+        'zlib1g-dev'
+        'libssl-dev'
+        'gperf'
+        'php-cli'
+        'cmake'
+        'g++'
+    )
 
-Write-Output 'Installing dependencies.'
-apt-get install -y @dependencies
-if (!$?) { throw 'Cannot install dependencies from apt-get' }
+    Write-Output 'Installing dependencies.'
+    apt-get install -y @dependencies
+    if (!$?) {
+        throw 'Cannot install dependencies from apt-get'
+    }
+} finally {
+    $env:DEBIAN_FRONTEND = $oldDebianFrontend
+}
 
 Write-Output "Downloading NuGet client to $NuGetPath"
 New-Item -Type Directory ([IO.Path]::GetDirectoryName($NuGetPath))
