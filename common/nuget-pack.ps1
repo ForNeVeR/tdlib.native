50fb8302cee7a171b8afd11e61a8bea5071abc8e
Friedrich von Never <friedrich@fornever.me>
2021-12-09 00:53:49 +0700

    Release: prepare v1.7.9.1

--- common/nuget-pack.ps1
+++ common/nuget-pack.ps1
@@ -1,5 +1,5 @@
 param (
-    [string] $Version = '1.7.9',
+    [string] $Version = '1.7.9.1',
     [string] $SrcDirectory = "$PSScriptRoot/..",
     [string] $BuildDirectory = "$SrcDirectory/build",
 
