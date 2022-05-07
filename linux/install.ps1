b179aa594d8fcc2a066694bf9aea3004ef2c6fb7
Friedrich von Never <friedrich@fornever.me>
2022-05-07 21:32:33 +0700

    (#24) CI: prepare release artifacts

--- linux/install.ps1
+++ linux/install.ps1
@@ -2,7 +2,8 @@ param (
     [string] $NuGetDownloadUrl = 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe',
     [string] $NuGetPath = "$PSScriptRoot/../tools/nuget.exe",
     [switch] $ForBuild,
-    [switch] $ForTests
+    [switch] $ForTests,
+    [switch] $ForRelease
 )
 
 $ErrorActionPreference = 'Stop'
@@ -62,7 +63,9 @@ if ($ForTests) {
     if (!$?) {
         throw 'Cannot execute apt-get update.'
     }
+}
 
+if ($ForTests -or $ForRelease) {
     Write-Output "Downloading NuGet client to $NuGetPath"
     New-Item -Type Directory ([IO.Path]::GetDirectoryName($NuGetPath))
     Invoke-WebRequest -OutFile $NuGetPath $NuGetDownloadUrl
