706665acef525ee6280f16cbbf750360835defca
Friedrich von Never <friedrich@fornever.me>
2023-09-17 13:16:31 +0200

    CI: try to resurrect Ubuntu 18.04 testing

--- linux/install.ps1
+++ linux/install.ps1
@@ -48,19 +48,24 @@ if ($ForTests) {
     }
 
     Write-Output 'Installing Mono.'
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
     if (!$?) {
-        throw 'Cannot execute apt-key adv.'
+        throw "Error exit code from gpg: $LASTEXITCODE."
     }
 
-    echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+    echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-bionic main" | tee /etc/apt/sources.list.d/mono-official-stable.list
     if (!$?) {
-        throw 'Cannot add a package source.'
+        throw "Cannot add a package source, exit code: $LASTEXITCODE."
     }
 
-    apt-get update && apt-get install -y mono-devel
+    apt update
     if (!$?) {
-        throw 'Cannot execute apt-get update.'
+        throw "Cannot execute apt update, exit code: $LASTEXITCODE."
+    }
+
+    apt install -y mono-devel
+    if (!$?) {
+        throw "Cannot execute apt install, exit code: $LASTEXITCODE."
     }
 
     Write-Output "Downloading NuGet client to $NuGetPath"
