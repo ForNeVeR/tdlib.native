7291bdda55271dbaa5279051fdaed7bdd277ebca
Friedrich von Never <friedrich@fornever.me>
2024-05-12 23:05:29 +0200

    CI: enable built artifacts cache

--- linux/build.ps1
+++ linux/build.ps1
@@ -1,54 +1,62 @@
 param (
     [string] $td = "$PSScriptRoot/../td",
-    [string] $InstallPrefix = "$PSScriptRoot/../build/install"
+    [string] $InstallPrefix = "$PSScriptRoot/../build/install",
+    [string] $CheckUpToDateScript = "$PSScriptRoot/../common/Test-UpToDate.ps1",
+    [switch] $SkipUpToDateCheck
 )
 
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
-if (-not (Test-Path $td/build)) {
-    New-Item -Type Directory $td/build
-}
-
-Push-Location $td/build
-try {
-    $cmakeArguments = @(
-        '-DCMAKE_BUILD_TYPE=Release'
-        "-DCMAKE_INSTALL_PREFIX:PATH=$InstallPrefix"
-        '..'
-    )
-    $cmakePrepareCrossCompilingArguments = @(
-        '--build'
-        '.'
-        '--target', 'prepare_cross_compiling'
-    )
-    $cmakeBuildArguments = @(
-        '--build'
-        '.'
-        '--target', 'install'
-    )
-
-    cmake @cmakeArguments
-    if (!$?) {
-        throw 'Cannot execute cmake'
+if ($SkipUpToDateCheck -or !$(& $CheckUpToDateScript)) {
+    if (-not (Test-Path $td/build)) {
+        New-Item -Type Directory $td/build
     }
 
-    cmake @cmakePrepareCrossCompilingArguments
-    if (!$?) {
-        throw 'Cannot execute cmake --build --target prepare_cross_compiling'
-    }
+    Push-Location $td/build
+    try {
+        $cmakeArguments = @(
+            '-DCMAKE_BUILD_TYPE=Release'
+            "-DCMAKE_INSTALL_PREFIX:PATH=$InstallPrefix"
+            '..'
+        )
+        $cmakePrepareCrossCompilingArguments = @(
+            '--build'
+            '.'
+            '--target', 'prepare_cross_compiling'
+        )
+        $cmakeBuildArguments = @(
+            '--build'
+            '.'
+            '--target', 'install'
+        )
 
-    Set-Location ..
-    php SplitSource.php
-    if (!$?) {
-        throw 'Cannot execute php SplitSource.php'
-    }
+        cmake @cmakeArguments
+        if (!$?) {
+            throw 'Cannot execute cmake'
+        }
 
-    Set-Location build
-    cmake @cmakeBuildArguments
-    if (!$?) {
-        throw 'Cannot execute cmake --build --target install'
+        cmake @cmakePrepareCrossCompilingArguments
+        if (!$?) {
+            throw 'Cannot execute cmake --build --target prepare_cross_compiling'
+        }
+
+        Set-Location ..
+        php SplitSource.php
+        if (!$?) {
+            throw 'Cannot execute php SplitSource.php'
+        }
+
+        Set-Location build
+        cmake @cmakeBuildArguments
+        if (!$?) {
+            throw 'Cannot execute cmake --build --target install'
+        }
+
+        & $CheckUpToDateScript -GenerateCheckResult
+    } finally {
+        Pop-Location
     }
-} finally {
-    Pop-Location
+} else {
+    Write-Host 'The build result is up to date.'
 }
