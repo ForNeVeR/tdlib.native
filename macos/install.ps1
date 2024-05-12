4046f81f1c8fedc404cda64355dae218bd2b22f2
Friedrich von Never <friedrich@fornever.me>
2024-05-12 21:30:22 +0200

    (#74) macOS: try removing the workaround for OpenSSL

--- macos/install.ps1
+++ macos/install.ps1
@@ -4,8 +4,3 @@ Set-StrictMode -Version Latest
 Write-Output 'Installing gperf from brew'
 brew install gperf
 if (!$?) { throw 'Cannot install gperf from brew' }
-
-# https://github.com/actions/virtual-environments/issues/4195
-Write-Output 'Setting up an OpenSSL symlink for CMake.'
-sh -c 'ln -sf $(brew --cellar openssl@1.1)/1.1* /usr/local/opt/openssl'
-if (!$?) { throw 'Cannot set up an OpenSSL symlink.' }
