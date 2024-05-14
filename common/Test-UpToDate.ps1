f7c2b1a942ebdb9de4f9f9ce6aaa31d561e47ff4
Friedrich von Never <friedrich@fornever.me>
2024-05-14 22:30:17 +0200

    CI: better build result caching strategy

--- common/Test-UpToDate.ps1
+++ common/Test-UpToDate.ps1
@@ -1,31 +1,31 @@
 param (
     [string] $RepoDirectory = "$PSScriptRoot/../td",
     [string] $ArtifactsDirectory = "$PSScriptRoot/../artifacts",
-    [switch] $GenerateCheckResult
+    [switch] $GenerateCacheKey,
+    [string] $CacheKeyFile = "$ArtifactsDirectory/cache-key.json"
 )
 
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
-$resultFile = "$ArtifactsDirectory/artifact.json"
 $inputs = [pscustomobject] @{
     InputCommitHash = (git --git-dir=$RepoDirectory/.git rev-parse HEAD)
 }
 
-if ($GenerateCheckResult) {
+if ($GenerateCacheKey) {
     if (!(Test-Path $ArtifactsDirectory)) {
         New-Item -Type Directory $ArtifactsDirectory | Out-Null
     }
 
-    $inputs | ConvertTo-Json | Set-Content $resultFile
-    Write-Host "Result cache file generated: `"$resultFile`"."
+    $inputs | ConvertTo-Json | Set-Content $CacheKeyFile
+    Write-Host "Result cache file generated: `"$CacheKeyFile`"."
     return $true
-} elseif (Test-Path $resultFile) {
-    $result = Get-Content $resultFile | ConvertFrom-Json
+} elseif (Test-Path $CacheKeyFile) {
+    $result = Get-Content $CacheKeyFile | ConvertFrom-Json
     $upToDate = $result.InputCommitHash -eq $inputs.InputCommitHash
     Write-Host "Cache found: cached commit hash is $($result.InputCommitHash), current commit hash is $($inputs.InputCommitHash). Up to date: $upToDate."
     return $upToDate
 } else {
-    Write-Host "Last result cache file not found: `"$resultFile`"."
+    Write-Host "Last result cache file not found: `"$CacheKeyFile`"."
     return $false
 }
