d46938e7833e9d4d9c0f3697563e02d12ac44ff6
Friedrich von Never <friedrich@fornever.me>
2024-05-14 22:38:40 +0200

    CI: skip the artifact preparation stage if the caches are ready

--- windows/prepare-artifacts.ps1
+++ windows/prepare-artifacts.ps1
@@ -1,13 +1,17 @@
 param (
     [string] $BuildRoot = "$PSScriptRoot/../td/build",
     [string] $BinLocation = "$BuildRoot/Release",
-    [string] $TargetLocation = "./artifacts/"
+    [string] $TargetLocation = "./artifacts/",
+    [string] $CheckUpToDateScript = "$PSScriptRoot/../common/Test-UpToDate.ps1",
+    [switch] $SkipUpToDateCheck
 )
 
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
-if (!(Test-Path -LiteralPath $TargetLocation -Type Container)) {
-    New-Item -Type Directory $TargetLocation
+if ($SkipUpToDateCheck -or !$(& $CheckUpToDateScript)) {
+    if (!(Test-Path -LiteralPath $TargetLocation -Type Container)) {
+        New-Item -Type Directory $TargetLocation
+    }
+    Copy-Item "$BinLocation/*.dll" $TargetLocation
 }
-Copy-Item "$BinLocation/*.dll" $TargetLocation
