a38bac77b7eab5cd7c1163d675449c5678d8ac8d
Friedrich von Never <friedrich@fornever.me>
2024-05-14 22:44:25 +0200

    CI: separate cache key and result key

--- linux/prepare-artifacts.ps1
+++ linux/prepare-artifacts.ps1
@@ -14,4 +14,5 @@ if ($SkipUpToDateCheck -or !$(& $CheckUpToDateScript)) {
         New-Item -Type Directory $TargetLocation
     }
     Copy-Item "$InstallPrefix/lib/*" $TargetLocation
+    & $CheckUpToDateScript -GenerateResultKey
 }
