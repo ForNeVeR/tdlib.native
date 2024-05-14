f88a877eb06ace7710c904250e4878da8f876c0c
Friedrich von Never <friedrich@fornever.me>
2024-05-14 22:38:40 +0200

    CI: skip the artifact preparation stage if the caches are ready

--- linux/prepare-artifacts.ps1
+++ linux/prepare-artifacts.ps1
@@ -1,13 +1,17 @@
 param (
     [string] $BuildRoot = "$PSScriptRoot/../build",
     [string] $InstallPrefix = "$BuildRoot/install",
-    [string] $TargetLocation = "$PSScriptRoot/../artifacts"
+    [string] $TargetLocation = "$PSScriptRoot/../artifacts",
+    [string] $CheckUpToDateScript = "$PSScriptRoot/../common/Test-UpToDate.ps1",
+    [switch] $SkipUpToDateCheck
 )
 
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
-if (!(Test-Path -LiteralPath $TargetLocation -Type Container)) {
-    New-Item -Type Directory $TargetLocation
+if ($SkipUpToDateCheck -or !$(& $CheckUpToDateScript)) {
+    if (!(Test-Path -LiteralPath $TargetLocation -Type Container)) {
+        New-Item -Type Directory $TargetLocation
+    }
+    Copy-Item "$InstallPrefix/lib/*" $TargetLocation
 }
-Copy-Item "$InstallPrefix/lib/*" $TargetLocation
--- macos/prepare-artifacts.ps1
+++ macos/prepare-artifacts.ps1
@@ -1,12 +1,16 @@
 param (
     [string] $BuildRoot = "$PSScriptRoot/../td/build",
-    [string] $TargetLocation = "./artifacts/"
+    [string] $TargetLocation = "./artifacts/",
+    [string] $CheckUpToDateScript = "$PSScriptRoot/../common/Test-UpToDate.ps1",
+    [switch] $SkipUpToDateCheck
 )
 
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
-if (!(Test-Path -LiteralPath $TargetLocation -Type Container)) {
-    New-Item -Type Directory $TargetLocation
+if ($SkipUpToDateCheck -or !$(& $CheckUpToDateScript)) {
+    if (!(Test-Path -LiteralPath $TargetLocation -Type Container)) {
+        New-Item -Type Directory $TargetLocation
+    }
+    Copy-Item "$BuildRoot/libtdjson.dylib" $TargetLocation
 }
-Copy-Item "$BuildRoot/libtdjson.dylib" $TargetLocation
