372120d9616f28e86b9aa4498eb003869f0000b2
Friedrich von Never <friedrich@fornever.me>
2024-05-18 19:57:07 +0200

    (#74) CI: update the GitHub Actions generation scripts

--- github-actions.fsx
+++ github-actions.fsx
@@ -9,9 +9,11 @@ open type Generaptor.Library.Patterns
 
 let mainBranch = "master"
 
-let linuxImage = "ubuntu-20.04"
-let macOsImage = "macos-11"
-let windowsImage = "windows-2019"
+let macOs11 = "macos-11"
+let macOs14 = "macos-14"
+let ubuntu20_04 = "ubuntu-20.04"
+let ubuntuLatest = "ubuntu-latest"
+let windows2019 = "windows-2019"
 
 let pwsh name script =
     step(name = name, shell = "pwsh", run = script)
@@ -29,10 +31,52 @@ let pwshWithResult name script producedFiles = [
 let prepareArtifacts os resultFileName =
     pwshWithResult "Prepare artifact" $"./{os}/prepare-artifacts.ps1" [resultFileName]
 
+let buildJobName platform arch = $"build-{platform}-{arch}"
+
+let checkoutWithSubmodules = step(name = "Checkout", uses = "actions/checkout@v4", options = Map.ofList [
+    "submodules", "true"
+])
+
+let platformToDotNet = function
+    | "linux" -> "linux"
+    | "macos" -> "osx"
+    | "windows" -> "win"
+    | other -> failwith $"Unknown platform {other}"
+
+let archToDotNet = function
+    | "x86-64" -> "x64"
+    | "aarch64" -> "arm64"
+    | other -> failwith $"Unknown architecture {other}"
+
+let platformBuildArtifactDirectory platform architecture =
+    $"./build/runtimes/{platformToDotNet platform}-{archToDotNet architecture}/native"
+
+let downloadArtifact platform architecture =
+    step(
+        name = $"Download artifact: {platform}.{architecture}",
+        uses = "actions/download-artifact@v4",
+        options = Map.ofList [
+            "name", $"tdlib.{platform}.{architecture}"
+            "path", platformBuildArtifactDirectory platform architecture
+        ]
+    )
+
+let setUpDotNetSdk = step(name = "Set up .NET SDK", uses = "actions/setup-dotnet@v4", options = Map.ofList [
+    "dotnet-version", "7.0.x"
+])
+
+let testEnv =
+    [
+        "DOTNET_NOLOGO", "1"
+        "DOTNET_CLI_TELEMETRY_OPTOUT", "1"
+        "NUGET_PACKAGES", "${{ github.workspace }}/.github/nuget-packages"
+        "PACKAGE_VERSION_BASE", "1.8.21"
+    ] |> Seq.map(fun (k, v) -> setEnv k v)
+
 type Workflows =
     static member BuildArtifacts(
         platform: string,
-        architecture: string,
+        arch: string,
         artifactFileName: string,
         ?installScript: string,
         ?buildScriptArgs: string
@@ -44,7 +88,7 @@ type Workflows =
              uses = "actions/cache@v4",
              options = Map.ofList [
                 "path", "artifacts"
-                "key", $"{platform}.{architecture}." + "${{ hashFiles('.github/cache-key.json') }}"
+                "key", $"{platform}.{arch}." + "${{ hashFiles('.github/cache-key.json') }}"
             ]
         )
         yield! installScript |> Option.toList |> Seq.map(pwsh "Install")
@@ -56,10 +100,66 @@ type Workflows =
         )
         yield! prepareArtifacts platform $"artifacts/{artifactFileName}"
         step(name = "Upload build result", uses = "actions/upload-artifact@v4", options = Map.ofList [
-            "name", $"tdlib.{platform}.{architecture}"
+            "name", $"tdlib.{platform}.{arch}"
             "path", "artifacts/*"
         ])
     ]
+
+    static member BuildJob(
+        image: string,
+        platform: string,
+        arch: string,
+        artifactFileName: string,
+        ?installScript: string,
+        ?buildScriptArgs: string
+    ) = job (buildJobName platform arch) [
+        runsOn image
+        checkoutWithSubmodules
+        yield! Workflows.BuildArtifacts(
+            platform = platform,
+            arch = arch,
+            artifactFileName = artifactFileName,
+            ?installScript = installScript,
+            ?buildScriptArgs = buildScriptArgs
+        )
+    ]
+
+    static member TestJob(
+        image: string,
+        platform: string,
+        arch: string,
+        testArgs: string,
+        ?installScript: string,
+        ?afterDownloadSteps: JobCreationCommand seq
+    ) = job $"test-{platform}-{arch}" [
+        needs(buildJobName platform arch)
+        runsOn image
+        yield! testEnv
+
+        checkoutWithSubmodules
+        yield! installScript |> Option.toList |> Seq.map(pwsh "Install")
+        downloadArtifact platform arch
+
+        yield! afterDownloadSteps |> Option.toList |> Seq.collect id
+
+        setUpDotNetSdk
+        pwsh "Pack NuGet" "dotnet pack -p:PackageVersion=${{ env.PACKAGE_VERSION_BASE }} --output build"
+        // TODO[#64]: Add ${{ github.run_id }} as a patch version
+        step(name = "NuGet cache", uses = "actions/cache@v4", options = Map.ofList [
+            "path", "${{ env.NUGET_PACKAGES }}"
+            "key", "${{ runner.os }}.nuget.${{ hashFiles('tdsharp/**/*.csproj') }}"
+        ])
+        pwsh "Test" $"./common/test.ps1 {testArgs}"
+    ]
+
+module Platform =
+    let Linux = "linux"
+    let MacOS = "macos"
+    let Windows = "windows"
+
+module Arch =
+    let AArch64 = "aarch64"
+    let X86_64 = "x86-64"
 
 let workflows = [
     workflow "main" [
@@ -69,158 +169,99 @@ let workflows = [
         onPullRequestTo mainBranch
         onSchedule(day = DayOfWeek.Saturday)
 
-        let checkoutWithSubmodules = step(name = "Checkout", uses = "actions/checkout@v4", options = Map.ofList [
-            "submodules", "true"
-        ])
+        Workflows.BuildJob(
+            image = ubuntu20_04,
+            platform = Platform.Linux,
+            arch = Arch.X86_64,
+            installScript = "./linux/install.ps1 -ForBuild",
+            artifactFileName = "libtdjson.so"
+        )
 
-        let setUpDotNetSdk = step(name = "Set up .NET SDK", uses = "actions/setup-dotnet@v4", options = Map.ofList [
-            "dotnet-version", "7.0.x"
-        ])
+        Workflows.BuildJob(
+            image = macOs14,
+            platform = Platform.MacOS,
+            arch = Arch.AArch64,
+            installScript = "./macos/install.ps1",
+            artifactFileName = "libtdjson.dylib"
+        )
 
-        job "build-linux" [
-            runsOn linuxImage
-            checkoutWithSubmodules
-            yield! Workflows.BuildArtifacts(
-                platform = "linux",
-                architecture = "x86-64",
-                installScript = "./linux/install.ps1 -ForBuild",
-                artifactFileName = "libtdjson.so"
-            )
-        ]
+        Workflows.BuildJob(
+            image = macOs11,
+            platform = Platform.MacOS,
+            arch = Arch.X86_64,
+            installScript = "./macos/install.ps1",
+            artifactFileName = "libtdjson.dylib"
+        )
 
-        job "build-macos" [
-            runsOn macOsImage
-            checkoutWithSubmodules
-            yield! Workflows.BuildArtifacts(
-                platform = "macos",
-                architecture = "x86-64",
-                installScript = "./macos/install.ps1",
-                artifactFileName = "libtdjson.dylib"
-            )
-        ]
+        Workflows.BuildJob(
+            image = windows2019,
+            platform = Platform.Windows,
+            arch = Arch.X86_64,
+            buildScriptArgs = @"-VcpkgToolchain c:\vcpkg\scripts\buildsystems\vcpkg.cmake",
+            artifactFileName = "tdjson.dll"
+        )
 
-        job "build-windows" [
-            runsOn windowsImage
-            checkoutWithSubmodules
-
-            yield! Workflows.BuildArtifacts(
-                platform = "windows",
-                architecture = "x86-64",
-                buildScriptArgs = @"-VcpkgToolchain c:\vcpkg\scripts\buildsystems\vcpkg.cmake",
-                artifactFileName = "tdjson.dll"
-            )
-        ]
-
-        let testEnv =
-            [
-                "DOTNET_NOLOGO", "1"
-                "DOTNET_CLI_TELEMETRY_OPTOUT", "1"
-                "NUGET_PACKAGES", "${{ github.workspace }}/.github/nuget-packages"
-                "PACKAGE_VERSION_BASE", "1.8.21"
-            ] |> Seq.map(fun (k, v) -> setEnv k v)
-
-        let platformToDotNet = function
-            | "linux" -> "linux"
-            | "macos" -> "osx"
-            | "windows" -> "win"
-            | other -> failwith $"Unknown platform {other}"
-
-        let archToDotNet = function
-            | "x86-64" -> "x64"
-            | other -> failwith $"Unknown architecture {other}"
-
-        let platformBuildArtifactDirectory platform architecture =
-            $"./build/runtimes/{platformToDotNet platform}-{archToDotNet architecture}/native"
-
-        let downloadPlatformArtifact platform architecture =
-            step(
-                name = $"Download platform artifact: {platform}",
-                uses = "actions/download-artifact@v4",
-                options = Map.ofList [
-                    "name", $"tdlib.{platform}.{architecture}"
-                    "path", platformBuildArtifactDirectory platform architecture
-                ]
-            )
         let downloadAndRepackArtifact platform architecture = [
-            downloadPlatformArtifact platform architecture
+            downloadArtifact platform architecture
             let dir = platformBuildArtifactDirectory platform architecture
             pwsh
                 $"Archive artifact for platform: {platform}"
                 $"Set-Location {dir} && zip -r $env:GITHUB_WORKSPACE/tdlib.{platform}.{architecture}.zip *"
         ]
 
-        job "test-linux" [
-            needs "build-linux"
-            runsOn linuxImage
-            yield! testEnv
+        Workflows.TestJob(
+            image = ubuntu20_04,
+            platform = Platform.Linux,
+            arch = Arch.X86_64,
+            installScript = "./linux/install.ps1 -ForTests",
+            testArgs = "-NuGet $env:GITHUB_WORKSPACE/tools/nuget.exe -UseMono"
+        )
 
-            checkoutWithSubmodules
-            pwsh "Install" "./linux/install.ps1 -ForTests"
-            downloadPlatformArtifact "linux" "x86-64"
-            setUpDotNetSdk
-            pwsh "Pack NuGet" "dotnet pack -p:PackageVersion=${{ env.PACKAGE_VERSION_BASE }} --output build"
-            // TODO[#64]: Add ${{ github.run_id }} as a patch version
-            step(name = "NuGet cache", uses = "actions/cache@v4", options = Map.ofList [
-                "path", "${{ env.NUGET_PACKAGES }}"
-                "key", "${{ runner.os }}.nuget.${{ hashFiles('tdsharp/**/*.csproj') }}"
-            ])
-            pwsh "Test" "./common/test.ps1 -NuGet $env:GITHUB_WORKSPACE/tools/nuget.exe -UseMono"
-        ]
+        Workflows.TestJob(
+            image = macOs14,
+            platform = Platform.MacOS,
+            arch = Arch.AArch64,
+            testArgs = "-NuGet nuget"
+        )
 
-        job "test-macos" [
-            needs "build-macos"
-            runsOn macOsImage
-            yield! testEnv
+        Workflows.TestJob(
+            image = macOs11,
+            platform = Platform.MacOS,
+            arch = Arch.X86_64,
+            testArgs = "-NuGet nuget"
+        )
 
-            checkoutWithSubmodules
-            downloadPlatformArtifact "macos" "x86-64"
-            setUpDotNetSdk
-            pwsh "Pack NuGet" "dotnet pack -p:PackageVersion=${{ env.PACKAGE_VERSION_BASE }} --output build"
-            // TODO[#64]: Add ${{ github.run_id }} as a patch version
-            step(name = "NuGet cache", uses = "actions/cache@v4", options = Map.ofList [
-                "path", "${{ env.NUGET_PACKAGES }}"
-                "key", "${{ runner.os }}.nuget.${{ hashFiles('tdsharp/**/*.csproj') }}"
-            ])
-            pwsh "Test" "./common/test.ps1 -NuGet nuget"
-        ]
-
-        job "test-windows" [
-            needs "build-windows"
-            runsOn windowsImage
-            yield! testEnv
-
-            checkoutWithSubmodules
-            downloadPlatformArtifact "windows" "x86-64"
-            step(name = "Cache downloads for Windows", uses = "actions/cache@v4", options = Map.ofList [
-                "path", "build/downloads"
-                "key", "${{ hashFiles('windows/install.ps1') }}"
-            ])
-
-            pwsh "Install dependencies" "./windows/install.ps1"
-
-            pwsh "Windows-specific testing" "./windows/test.ps1"
-
-            setUpDotNetSdk
-            pwsh "Pack NuGet" "dotnet pack -p:PackageVersion=${{ env.PACKAGE_VERSION_BASE }} --output build"
-            // TODO[#64]: Add ${{ github.run_id }} as a patch version
-            step(name = "NuGet cache", uses = "actions/cache@v4", options = Map.ofList [
-                "path", "${{ env.NUGET_PACKAGES }}"
-                "key", "${{ runner.os }}.nuget.${{ hashFiles('tdsharp/**/*.csproj') }}"
-            ])
-            pwsh "Test" "./common/test.ps1 -NuGet nuget"
-        ]
+        Workflows.TestJob(
+            image = windows2019,
+            platform = Platform.Windows,
+            arch = Arch.X86_64,
+            testArgs = "-NuGet nuget",
+            afterDownloadSteps = [
+                step(name = "Cache downloads for Windows", uses = "actions/cache@v4", options = Map.ofList [
+                    "path", "build/downloads"
+                    "key", "${{ hashFiles('windows/install.ps1') }}"
+                ])
+                pwsh "Install dependencies" "./windows/install.ps1"
+                pwsh "Windows-specific testing" "./windows/test.ps1"
+            ]
+        )
 
         job "release" [
-            runsOn linuxImage
-            needs "build-linux"
-            needs "build-macos"
-            needs "build-windows"
+            runsOn ubuntuLatest
+            yield! [
+                buildJobName Platform.Linux Arch.X86_64
+                buildJobName Platform.MacOS Arch.AArch64
+                buildJobName Platform.MacOS Arch.X86_64
+                buildJobName Platform.Windows Arch.X86_64
+            ] |> Seq.map needs
+
             yield! testEnv
             checkout
 
-            yield! downloadAndRepackArtifact "linux" "x86-64"
-            yield! downloadAndRepackArtifact "macos" "x86-64"
-            yield! downloadAndRepackArtifact "windows" "x86-64"
+            yield! downloadAndRepackArtifact Platform.Linux Arch.X86_64
+            yield! downloadAndRepackArtifact Platform.MacOS Arch.AArch64
+            yield! downloadAndRepackArtifact Platform.MacOS Arch.X86_64
+            yield! downloadAndRepackArtifact Platform.Windows Arch.X86_64
 
             setUpDotNetSdk
 
@@ -274,9 +315,9 @@ let workflows = [
                     ]
                 )
 
-                let releasePlatformArtifact platform architecture =
+                let releaseArtifact platform architecture =
                     step(
-                        name = $"Release platform artifact: {platform}",
+                        name = $"Release artifact: {platform}.{architecture}",
                         uses = "actions/upload-release-asset@v1",
                         env = Map.ofList [
                             "GITHUB_TOKEN", "${{ secrets.GITHUB_TOKEN }}"
@@ -289,9 +330,11 @@ let workflows = [
                         ]
                     )
 
-                releasePlatformArtifact "linux" "x86-64"
-                releasePlatformArtifact "macos" "x86-64"
-                releasePlatformArtifact "windows" "x86-64"
+                releaseArtifact Platform.Linux Arch.X86_64
+                releaseArtifact Platform.MacOS Arch.AArch64
+                releaseArtifact Platform.MacOS Arch.X86_64
+                releaseArtifact Platform.Windows Arch.X86_64
+
                 step(
                     name = "Release NuGet package",
                     uses = "actions/upload-release-asset@v1",
